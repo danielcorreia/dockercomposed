8909d98a61499b2aee954d62a6ac4ea023a1527a
Daniel Correia <daniel@danielcorreia.pt>
2018-04-14 14:42:41 +0100

    Change indentation

--- web/server.js
+++ web/server.js
@@ -1,20 +1,18 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const path = require('path')
 
 const config = {
-	PORT: 3000,
-	HOST: '0.0.0.0',
+  PORT: 3000,
+  HOST: "0.0.0.0"
 };
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.get('/health', (req, res) => {
-	res.json({name: 'web', statusOk: true});
+app.get("/health", (req, res) => {
+  res.json({ name: "web", statusOk: true });
 });
 
-app.listen(
-	config.PORT,
-	config.HOST,
-	() => console.log(`Running on port ${config.HOST}:${config.PORT}!`)
+app.listen(config.PORT, config.HOST, () =>
+  console.log(`Running on port ${config.HOST}:${config.PORT}!`)
 );
